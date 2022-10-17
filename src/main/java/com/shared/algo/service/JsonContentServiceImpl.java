d2b8b45bbcc31d4faea757efb1a3307daf356062
Suhas H C <suhashc193@gmail.com>
2022-10-17 18:57:45 +0530

    added-logger to service file

--- src/main/java/com/shared/algo/service/JsonContentServiceImpl.java
+++ src/main/java/com/shared/algo/service/JsonContentServiceImpl.java
@@ -3,6 +3,8 @@ package com.shared.algo.service;
 import java.io.InputStream;
 import java.util.Collection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -11,6 +13,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class JsonContentServiceImpl implements JsonContentService {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(JsonContentServiceImpl.class);
+	
 	@Override
 	public Object fetchJsonData(Class<?> clazz, String path) throws Exception {
 		TypeReference<Collection<?>> data = new TypeReference<Collection<?>>() {};
