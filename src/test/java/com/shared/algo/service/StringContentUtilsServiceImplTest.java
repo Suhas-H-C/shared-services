e8637ad157c8da482f94e3934a61deb0e2f7ee7c
Suhas H C <suhashc193@gmail.com>
2022-10-27 12:29:28 +0530

    Updated test cases and intendations

--- src/test/java/com/shared/algo/service/StringContentUtilsServiceImplTest.java
+++ src/test/java/com/shared/algo/service/StringContentUtilsServiceImplTest.java
@@ -1,28 +1,36 @@
 package com.shared.algo.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Collection;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.shared.algo.model.IpData;
+
 @ExtendWith(MockitoExtension.class)
 class StringContentUtilsServiceImplTest {
 
-    @InjectMocks
-    private StringContentUtilsServiceImpl stringContentUtilsImpl;
+	@InjectMocks
+	private StringContentUtilsServiceImpl stringContentUtilsImpl;
 
-    @Test
-    @DisplayName("testGetContent_IllegalArgumentException")
-    void testGetContent() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            stringContentUtilsImpl.getContent("demo");
-        });
-    }
+	@Test
+	@DisplayName("testGetContent_IllegalArgumentException")
+	void testGetContent() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			stringContentUtilsImpl.getContent("demo");
+		});
+	}
 
-    @Test
-    void testGetFields() {
-    }
+	@Test
+	@DisplayName("testGetFields_False")
+	void testGetFields() {
+		Collection<?> feildData = stringContentUtilsImpl.getFields(new IpData());
+		assertFalse(feildData.isEmpty());
+	}
 
 }
