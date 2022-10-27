a1166a1a010bde71028d6974d313f1407c9a25c0
Suhas H C <suhashc193@gmail.com>
2022-10-27 20:29:05 +0530

    tried adding task

--- src/test/java/com/shared/algo/service/CsvServiceImplTest.java
+++ src/test/java/com/shared/algo/service/CsvServiceImplTest.java
@@ -1,18 +1,15 @@
 package com.shared.algo.service;
 
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
 
-import org.apache.commons.csv.CSVParser;
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 
@@ -21,19 +18,17 @@ import com.fasterxml.jackson.core.type.TypeReference;
 @ExtendWith(MockitoExtension.class)
 class CsvServiceImplTest {
 
-	
 	@InjectMocks
 	private CsvServiceImpl csvServiceImpl;
-	
-	
+
 	@Test
 	@DisplayName("readCsvFileTest")
 	void readCsvFileTest() throws Exception {
 		InputStream in = TypeReference.class.getResourceAsStream("/data/files/MOCK_DATA_TEST.csv");
-		
+
 		MockMultipartFile multipartFile = new MockMultipartFile("test", in);
 		Collection<?> data = csvServiceImpl.retrieveData(multipartFile);
-		
+
 		assertFalse(data.isEmpty());
 		assertEquals(10, data.size());
 	}
