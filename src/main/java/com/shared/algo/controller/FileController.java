7c3a097092d1d0f2e2006f6d76dbc4abcb5cf3b8
Suhas H C <suhashc193@gmail.com>
2022-10-29 15:45:06 +0530

    Added excel reading and writing functionality with refactoring and API

--- src/main/java/com/shared/algo/controller/FileController.java
+++ src/main/java/com/shared/algo/controller/FileController.java
@@ -32,97 +32,117 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 @RequestMapping(value = "/file")
 public class FileController {
 
-    @Autowired
-    private StringContentUtilsService stringContentUtils;
-    @Autowired
-    private JsonContentService jsonContentService;
-    @Autowired
-    private CsvService csvService;
-    @Autowired
-    private XlsxService xlsxService;
+	@Autowired
+	private StringContentUtilsService stringContentUtils;
+	@Autowired
+	private JsonContentService jsonContentService;
+	@Autowired
+	private CsvService csvService;
+	@Autowired
+	private XlsxService xlsxService;
 
-    @Operation(method = "GET", description = "Retrieves the string from file", tags = "file")
-    @ApiResponses({@ApiResponse(responseCode = "200", description = "Success"),
-            @ApiResponse(responseCode = "400", description = "Bad Request"),
-            @ApiResponse(responseCode = "401", description = "Unauthorized"),
-            @ApiResponse(responseCode = "403", description = "Forbidden"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
-    @GetMapping(value = "/fetch-content")
-    public ResponseEntity<GenericResponse<?>> getContent(@RequestParam(name = "fileName", required = true) String fileName) {
-        try {
-            return new ResponseEntity<>
-                    (wrapWithGenericResponse(stringContentUtils.getContent(fileName)), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(wrapWithGenericResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
-        }
-    }
+	@Operation(method = "GET", description = "Retrieves the string from file", tags = "file")
+	@ApiResponses({ @ApiResponse(responseCode = "200", description = "Success"),
+			@ApiResponse(responseCode = "400", description = "Bad Request"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized"),
+			@ApiResponse(responseCode = "403", description = "Forbidden"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error") })
+	@GetMapping(value = "/fetch-content")
+	public ResponseEntity<GenericResponse<?>> getContent(
+			@RequestParam(name = "fileName", required = true) String fileName) {
+		try {
+			return new ResponseEntity<>(wrapWithGenericResponse(stringContentUtils.getContent(fileName)),
+					HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(wrapWithGenericResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+		}
+	}
 
+	@Operation(method = "GET", description = "Retrieves the json from file", tags = "file")
+	@ApiResponses({ @ApiResponse(responseCode = "200", description = "Success"),
+			@ApiResponse(responseCode = "400", description = "Bad Request"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized"),
+			@ApiResponse(responseCode = "403", description = "Forbidden"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error") })
+	@GetMapping(value = "/fetch-json")
+	public ResponseEntity<GenericResponse<?>> getJson(
+			@RequestParam(name = "stringPath", required = true) String stringPath) {
+		try {
+			return new ResponseEntity<>(
+					wrapWithGenericResponse(jsonContentService.fetchJsonData(IpData.class, stringPath)), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(wrapWithGenericResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+		}
+	}
 
-    @Operation(method = "GET", description = "Retrieves the json from file", tags = "file")
-    @ApiResponses({@ApiResponse(responseCode = "200", description = "Success"),
-            @ApiResponse(responseCode = "400", description = "Bad Request"),
-            @ApiResponse(responseCode = "401", description = "Unauthorized"),
-            @ApiResponse(responseCode = "403", description = "Forbidden"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
-    @GetMapping(value = "/fetch-json")
-    public ResponseEntity<GenericResponse<?>> getJson(@RequestParam(name = "stringPath", required = true) String stringPath) {
-        try {
-            return new ResponseEntity<>
-                    (wrapWithGenericResponse(jsonContentService.fetchJsonData(IpData.class, stringPath)), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(wrapWithGenericResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
-        }
-    }
+	@Operation(method = "POST", description = "Retrieves the data from CSV file", tags = "csv-file")
+	@ApiResponses({ @ApiResponse(responseCode = "200", description = "Success"),
+			@ApiResponse(responseCode = "400", description = "Bad Request"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized"),
+			@ApiResponse(responseCode = "403", description = "Forbidden"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error") })
+	@PostMapping(value = "/read-csv", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<GenericResponse<?>> readCSV(
+			@RequestParam(name = "file", required = true) MultipartFile multipartFile) {
+		try {
+			return new ResponseEntity<>(wrapWithGenericResponse(csvService.retrieveData(multipartFile, IpData.class)),
+					HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(wrapWithGenericResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+		}
+	}
 
+	@Operation(method = "GET", description = "Produces CSV file", tags = "csv-file")
+	@ApiResponses({ @ApiResponse(responseCode = "200", description = "Success"),
+			@ApiResponse(responseCode = "400", description = "Bad Request"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized"),
+			@ApiResponse(responseCode = "403", description = "Forbidden"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error") })
+	@GetMapping(value = "/produce-csv", produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<?> getCSV(
+			@RequestParam(value = "fileName", required = true, defaultValue = "data") String fileName) {
+		try {
+			return ResponseEntity.ok()
+					.header(DISPOSITION.getKey(), DISPOSITION.getContent().concat(fileName).concat(".csv"))
+					.body(new InputStreamResource(
+							csvService.getCSV((List<?>) jsonContentService.fetchJsonData(IpData.class, "ipData"))));
+		} catch (Exception e) {
+			return new ResponseEntity<>(wrapWithGenericResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+		}
+	}
 
-    @Operation(method = "POST", description = "Retrieves the data from CSV file", tags = "csv-file")
-    @ApiResponses({@ApiResponse(responseCode = "200", description = "Success"),
-            @ApiResponse(responseCode = "400", description = "Bad Request"),
-            @ApiResponse(responseCode = "401", description = "Unauthorized"),
-            @ApiResponse(responseCode = "403", description = "Forbidden"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
-    @PostMapping(value = "/read-csv", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<GenericResponse<?>> readCSV(@RequestParam(name = "file", required = true) MultipartFile multipartFile) {
-        try {
-            return new ResponseEntity<>
-                    (wrapWithGenericResponse(csvService.retrieveData(multipartFile, IpData.class)), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(wrapWithGenericResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
-        }
-    }
+	@Operation(method = "GET", description = "Reads Excel file", tags = "xlsx-file")
+	@ApiResponses({ @ApiResponse(responseCode = "200", description = "Success"),
+			@ApiResponse(responseCode = "400", description = "Bad Request"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized"),
+			@ApiResponse(responseCode = "403", description = "Forbidden"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error") })
+	@GetMapping(value = "/read-xlsx", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> readExcel(@RequestParam(value = "file", required = true) MultipartFile multipartFile) {
+		try {
+			return new ResponseEntity<>(wrapWithGenericResponse(xlsxService.read(multipartFile, IpData.class)),
+					HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(wrapWithGenericResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+		}
+	}
 
-
-    @Operation(method = "GET", description = "Produces CSV file", tags = "csv-file")
-    @ApiResponses({@ApiResponse(responseCode = "200", description = "Success"),
-            @ApiResponse(responseCode = "400", description = "Bad Request"),
-            @ApiResponse(responseCode = "401", description = "Unauthorized"),
-            @ApiResponse(responseCode = "403", description = "Forbidden"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
-    @GetMapping(value = "/produce-csv", produces = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> getCSV(@RequestParam(value = "fileName", required = true, defaultValue = "data") String fileName) {
-        try {
-            return ResponseEntity.ok()
-                    .header(DISPOSITION.getKey(), DISPOSITION.getContent().concat(fileName).concat(".csv"))
-                    .body(new InputStreamResource(csvService.getCSV((List<?>) jsonContentService.fetchJsonData(IpData.class, "ipData"))));
-        } catch (Exception e) {
-            return new ResponseEntity<>(wrapWithGenericResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
-        }
-    }
-    
-    
-    @Operation(method = "GET", description = "Reads Excel file", tags = "xlsx-file")
-    @ApiResponses({@ApiResponse(responseCode = "200", description = "Success"),
-            @ApiResponse(responseCode = "400", description = "Bad Request"),
-            @ApiResponse(responseCode = "401", description = "Unauthorized"),
-            @ApiResponse(responseCode = "403", description = "Forbidden"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
-    @GetMapping(value = "/read-xlsx",consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> readExcel(@RequestParam(value = "file", required = true) MultipartFile multipartFile) {
-        try {
-            return new ResponseEntity<>(wrapWithGenericResponse(xlsxService.read(multipartFile, IpData.class)), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(wrapWithGenericResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
-        }
-    }
+	@Operation(method = "GET", description = "Produces Excel file", tags = "xlsx-file")
+	@ApiResponses({ @ApiResponse(responseCode = "200", description = "Success"),
+			@ApiResponse(responseCode = "400", description = "Bad Request"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized"),
+			@ApiResponse(responseCode = "403", description = "Forbidden"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error") })
+	@GetMapping(value = "/produce-xlsx", produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<?> getExcel(@RequestParam(value = "fileName", required = true) String fileName) {
+		try {
+			return ResponseEntity.ok()
+					.header(DISPOSITION.getKey(), DISPOSITION.getContent().concat(fileName).concat(".xlsx"))
+					.body(new InputStreamResource(
+							xlsxService.write((List<?>) jsonContentService.fetchJsonData(IpData.class, "ipData"))));
+		} catch (Exception e) {
+			return new ResponseEntity<>(wrapWithGenericResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+		}
+	}
 
 }
--- src/main/java/com/shared/algo/service/XlsxServiceImpl.java
+++ src/main/java/com/shared/algo/service/XlsxServiceImpl.java
@@ -1,6 +1,11 @@
 package com.shared.algo.service;
 
+import static com.shared.algo.enums.FileConstants.SHEET_NAME;
+import static com.shared.algo.enums.Messages.INVLAID_SHEET;
+
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -14,8 +19,8 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import static com.shared.algo.enums.FileConstants.SHEET_NAME;
-import static com.shared.algo.enums.Messages.*;
+import com.shared.algo.annotations.Headers;
+import com.shared.algo.enums.Messages;
 import com.shared.algo.exception.BadRequestException;
 import com.shared.algo.model.IpData;
 
@@ -26,34 +31,34 @@ public class XlsxServiceImpl implements XlsxService {
 	public Collection<?> read(MultipartFile multipartFile, Class<?> clazz) throws Exception {
 		List<Object> response = new ArrayList<>();
 
-		try (Workbook workbook = new XSSFWorkbook(multipartFile.getInputStream())){
+		try (Workbook workbook = new XSSFWorkbook(multipartFile.getInputStream())) {
 			Sheet sheet = workbook.getSheet(SHEET_NAME.getValue());
-			if(Objects.isNull(sheet)) {
+			if (Objects.isNull(sheet)) {
 				throw new BadRequestException(INVLAID_SHEET.getMessage());
-			}else {
+			} else {
 				for (Row row : sheet) {
-					if(row.getRowNum() == 0) {
+					if (row.getRowNum() == 0) {
 						continue;
-					}else {
-						
-						if(clazz.isInstance(new IpData())) {
+					} else {
+
+						if (clazz.isInstance(new IpData())) {
 							IpData data = BeanUtils.instantiateClass(IpData.class);
-							
+
 							data.setId((int) row.getCell(0).getNumericCellValue());
 							data.setFirst_name(row.getCell(1).getStringCellValue());
 							data.setLast_name(row.getCell(2).getStringCellValue());
 							data.setEmail(row.getCell(3).getStringCellValue());
 							data.setGender(row.getCell(4).getStringCellValue());
 							data.setIp_address(row.getCell(5).getStringCellValue());
-							
+
 							response.add(data);
-						}else {
+						} else {
 							break;
 						}
 					}
 				}
 			}
-			
+
 			return response;
 		} catch (Exception e) {
 			throw new BadRequestException(e.getMessage());
@@ -62,7 +67,47 @@ public class XlsxServiceImpl implements XlsxService {
 
 	@Override
 	public ByteArrayInputStream write(List<?> data) throws Exception {
-		// TODO Auto-generated method stub
+		try (Workbook workBook = new XSSFWorkbook();
+				ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
+			Sheet sheet = workBook.createSheet(SHEET_NAME.getValue());
+
+			if (Objects.nonNull(data) && !data.isEmpty()) {
+				List<String> fileHeaders = new ArrayList<>();
+
+				if (data.get(0) instanceof IpData ipDataInstance) {
+					Field[] fields = ipDataInstance.getClass().getDeclaredFields();
+					for (Field field : fields) {
+						Headers headers = field.getAnnotation(Headers.class);
+						fileHeaders.add(headers.header());
+					}
+					Row headerRow = sheet.createRow(0);
+					for (int i = 0; i < fileHeaders.size(); i++) {
+						headerRow.createCell(i).setCellValue(fileHeaders.get(i));
+					}
+					int flag = 1;
+
+					for (Object obj : data) {
+						IpData ipData = (IpData) obj;
+						Row valueRow = sheet.createRow(flag++);
+						valueRow.createCell(0).setCellValue(ipData.getId());
+						valueRow.createCell(1).setCellValue(ipData.getFirst_name());
+						valueRow.createCell(2).setCellValue(ipData.getLast_name());
+						valueRow.createCell(3).setCellValue(ipData.getEmail());
+						valueRow.createCell(4).setCellValue(ipData.getGender());
+						valueRow.createCell(5).setCellValue(ipData.getIp_address());
+					}
+				} else {
+					throw new BadRequestException(Messages.TYPE_NOT_FOUND.getMessage());
+				}
+
+				workBook.write(byteArrayOutputStream);
+				workBook.close();
+				return new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+			}
+
+		} catch (Exception e) {
+			throw new BadRequestException(e.getMessage());
+		}
 		return null;
 	}
 
