ec0d8c8dd4e96f22a8066a39db6d2d894c7a9068
Suhas H C <suhashc193@gmail.com>
2022-10-29 18:01:41 +0530

    added dynamic file name and refactored

--- src/main/java/com/shared/algo/service/PDFServiceImpl.java
+++ src/main/java/com/shared/algo/service/PDFServiceImpl.java
@@ -11,42 +11,39 @@ import com.lowagie.text.FontFactory;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
+import static com.shared.algo.enums.FileConstants.PDF_TITLE;
 
 @Service
 public class PDFServiceImpl implements PDFService {
 
 	@Autowired
 	private StringContentUtilsService stringContentUtilsService;
-	
+
 	@Override
 	public void write(HttpServletResponse response) throws Exception {
 		Document document = new Document(PageSize.A4);
-		
+
 		PdfWriter.getInstance(document, response.getOutputStream());
-		
-		if(!document.isOpen()) {
+
+		if (!document.isOpen()) {
 			document.open();
-			
+
 			Font titleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
 			titleFont.setSize(18);
-			Paragraph titleText = new Paragraph("Shared-Algos", titleFont);
+			Paragraph titleText = new Paragraph(PDF_TITLE.getValue(), titleFont);
 			titleText.setAlignment(Paragraph.ALIGN_CENTER);
-			
+
 			Font titleBody = FontFactory.getFont(FontFactory.COURIER);
 			titleBody.setSize(10);
-			Paragraph bodyText = new Paragraph(stringContentUtilsService.getContent("PDFContent.txt"),
-					titleBody);
+			Paragraph bodyText = new Paragraph(stringContentUtilsService.getContent("PDFContent.txt"), titleBody);
 			bodyText.setAlignment(Paragraph.ALIGN_LEFT);
-			
+
 			document.add(titleText);
 			document.add(bodyText);
-			
+
 			document.close();
 		}
-		
-		
-		
-		
+
 	}
 
 }
