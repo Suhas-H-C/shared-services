fef34674bb854df1a90618f4eebb116986d19bcd
Suhas H C <suhashc193@gmail.com>
2022-10-29 19:12:02 +0530

    Generified logic

--- src/main/java/com/shared/algo/service/JsonContentServiceImpl.java
+++ src/main/java/com/shared/algo/service/JsonContentServiceImpl.java
@@ -2,10 +2,13 @@ package com.shared.algo.service;
 
 import java.io.InputStream;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -16,16 +19,25 @@ import com.shared.algo.model.IpData;
 public class JsonContentServiceImpl implements JsonContentService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(JsonContentServiceImpl.class);
-	
+
 	@Override
 	public Collection<?> fetchJsonData(Class<?> clazz, String path) throws Exception {
-		TypeReference<List<IpData>> data = new TypeReference<List<IpData>>() {};
-		InputStream in = 
-				TypeReference.class.getResourceAsStream("/data/json/" + path + ".json");
 		
-		ObjectMapper objectMapper = new ObjectMapper();
-		List<IpData> value = objectMapper.readValue(in, data);
-		LOGGER.info("Data size : {}", value.size());
+		if(clazz.isInstance(new IpData()) && Objects.nonNull(path)) {
+			TypeReference<List<IpData>> data = new TypeReference<>() {
+			};
+			Object jsonResponse = processJsonRequest(data, path);
+			LOGGER.info("Processing completed");
+			return Collections.singleton(jsonResponse);
+		}
+		return null;
+	}
+
+	private static final Object processJsonRequest(TypeReference<?> typeReference, String path) throws Exception {
+		InputStream in = TypeReference.class.getResourceAsStream("/data/json/" + path + ".json");
+
+		ObjectMapper objectMapper = BeanUtils.instantiateClass(ObjectMapper.class);
+		Object value = objectMapper.readValue(in, typeReference);
 		return value;
 	}
 
