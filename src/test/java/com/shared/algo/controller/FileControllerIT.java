ac3a20cebf967e8d8c95906f7877d610a0122b9b
Suhas H C <suhashc193@gmail.com>
2022-12-20 17:07:26 +0530

    Added tests and refactoring.

--- src/test/java/com/shared/algo/controller/FileControllerIT.java
+++ src/test/java/com/shared/algo/controller/FileControllerIT.java
@@ -8,10 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FileControllerIT {
@@ -20,6 +28,9 @@ public class FileControllerIT {
 
     @LocalServerPort
     String port;
+
+    @Autowired
+    private FileController fileController;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -42,10 +53,41 @@ public class FileControllerIT {
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
+    @Test
+    void getJsonIT() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.valueOf(MediaType.APPLICATION_JSON_VALUE));
+        headers.setBasicAuth("suhas", "suhas");
+        HttpEntity<?> httpEntity = new HttpEntity<>(headers);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(buildUrl("/file/fetch-json"));
+        builder.queryParam("stringPath", "ipData");
+
+        ResponseEntity<GenericResponse> response =
+                restTemplate.exchange(builder.toUriString(),
+                        HttpMethod.GET, httpEntity, GenericResponse.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    void readCsvIT() throws IOException {
+        ResponseEntity<GenericResponse<?>> response = fileController.readCSV(multipartFile("MOCK_DATA_TEST.csv"), "ipdata");
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(requireNonNull(response.getBody()).getData());
+    }
+
 
     private String buildUrl(String contextPath) {
         String url = "http://localhost:" + port + contextPath;
         LOG.info("URL : {}", url);
         return url;
+    }
+
+    private MultipartFile multipartFile(String path) throws IOException {
+        File file = new File("src/main/resources/data/files/" + path);
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        return new MockMultipartFile(file.getName(), fileInputStream);
     }
 }
