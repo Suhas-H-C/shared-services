c815bed21948f6b8abcb9dfa636a74d391ce94a6
Suhas H C <suhashc193@gmail.com>
2023-04-20 11:29:02 +0530

    Added binary search

---
+++ src/main/java/com/shared/algo/utils/dsa/BinarySearch.java
@@ -0,0 +1,71 @@
+package com.shared.algo.utils.dsa;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BinarySearch {
+
+    public int searchForTargetFirstOcc(int[] a, int t) {
+        int left = 0, right = a.length - 1, first = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (a[mid] == t) {
+                first = mid;
+                right = right - 1;
+            } else if (a[mid] < t) {
+                left = left + 1;
+            } else {
+                right = right - 1;
+            }
+        }
+        return first;
+    }
+
+    public int searchForTargetLastOcc(int[] a, int t) {
+        int left = 0, right = a.length - 1, last = 0;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (a[mid] == t) {
+                last = mid;
+                left = left + 1;
+            } else if (a[mid] < t) {
+                left = left + 1;
+            } else {
+                right = right - 1;
+            }
+        }
+
+        return last;
+    }
+
+    public void searchForTargetFirstAndLastOcc(int[] a, int t) {
+        int left = 0, right = a.length - 1, first = 0;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (a[mid] == t) {
+                first = mid;
+                right = right - 1;
+            } else if (a[mid] < t) {
+                left = left + 1;
+            } else {
+                right = right - 1;
+            }
+        }
+
+        left = 0;
+        right = a.length - 1;
+        int last = 0;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (a[mid] == t) {
+                last = mid;
+                left = left + 1;
+            } else if (a[mid] < t) {
+                left = left + 1;
+            } else {
+                right = right - 1;
+            }
+        }
+        System.out.println("First occurrence " + first + " last occurrence " + last);
+    }
+}
