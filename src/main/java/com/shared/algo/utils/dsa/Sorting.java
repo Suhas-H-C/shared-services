9eee4799bff77965a575de2d6cf0b4d7f576157a
Suhas H C <suhashc193@gmail.com>
2023-04-24 19:54:22 +0530

    Added array DSA and test cases

---
+++ src/main/java/com/shared/algo/utils/dsa/Sorting.java
@@ -0,0 +1,57 @@
+package com.shared.algo.utils.dsa;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class Sorting {
+
+    //Ascending order
+    public int[] selectionSort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            int minVal = a[i];
+            int minInd = i;
+
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < minVal) {
+                    minVal = a[j];
+                    minInd = j;
+                }
+            }
+
+            int temp = a[i];
+            a[i] = a[minInd];
+            a[minInd] = temp;
+        }
+        return a;
+    }
+
+    //Ascending order
+    public int[] bubbleSort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        return a;
+    }
+
+    //Descending order
+    public int[] bubbleSorted(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - 1 - i; j++) {
+                if (a[j] < a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        return a;
+    }
+}
