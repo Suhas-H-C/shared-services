244eca8e153c7fa0f68edddec9db2b8a5b3a508c
Suhas H C <suhashc193@gmail.com>
2023-07-14 14:54:36 +0530

    Added parameterized tests and used BiPredicates

---
+++ src/main/java/com/shared/algo/service/ClientService.java
@@ -0,0 +1,8 @@
+package com.shared.algo.service;
+
+import com.shared.algo.model.ClientDetails;
+
+public interface ClientService {
+
+    ClientDetails getClientDetails(String referenceNumber);
+}
---
+++ src/main/java/com/shared/algo/service/ClientServiceImpl.java
@@ -0,0 +1,13 @@
+package com.shared.algo.service;
+
+import com.shared.algo.model.ClientDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientServiceImpl implements ClientService{
+
+    @Override
+    public ClientDetails getClientDetails(String referenceNumber) {
+        return null;
+    }
+}
