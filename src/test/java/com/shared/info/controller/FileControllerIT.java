0e46326c1aba4ff8069d53f7cbc911b7ab3ebb5f
Suhas H C <suhashc193@gmail.com>
2023-09-18 21:17:46 +0530

    IT test cases added for parsing csv file

--- src/test/java/com/shared/info/controller/FileControllerIT.java
+++ src/test/java/com/shared/info/controller/FileControllerIT.java
@@ -7,11 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,5 +42,20 @@ class FileControllerIT {
         MvcResult actualResponse = mockMvc.perform(get("/file/fetch-json").param("stringPath", "InternetProtocol")).andDo(print()).andExpect(status().isOk()).andReturn();
         log.info("{}", actualResponse.getResponse().getBufferSize());
         assertEquals(HttpStatus.OK.value(), actualResponse.getResponse().getStatus());
+    }
+
+    @Test
+    void should_parse_csv_file_and_return_valid_response_when_file_name_is_passed() throws Exception {
+        MvcResult actualResponse = mockMvc.perform(multipart("/file/read-csv").file(multipartFile("MOCK_DATA_TEST.csv")).param("type", "ipdata")).andDo(print())
+                .andExpect(status().isOk()).andReturn();
+
+        log.info("{}", actualResponse.getResponse().getBufferSize());
+        assertEquals(HttpStatus.OK.value(), actualResponse.getResponse().getStatus());
+    }
+
+    private static MockMultipartFile multipartFile(String path) throws IOException {
+        File file = new File("src/main/resources/data/files/" + path);
+        FileInputStream inputStream = new FileInputStream(file);
+        return new MockMultipartFile("file", path, MediaType.MULTIPART_FORM_DATA_VALUE, inputStream);
     }
 }
