e1bbe1ae3d62e0e974ab0c9679ed13e93195a63c
Suhas H C <suhashc193@gmail.com>
2023-09-20 22:02:27 +0530

    added feign dependency by failing

--- src/main/java/com/shared/info/SharedServicesApplication.java
+++ src/main/java/com/shared/info/SharedServicesApplication.java
@@ -6,11 +6,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 
 @Slf4j
 @AllArgsConstructor
 @SpringBootApplication
+@EnableFeignClients
 public class SharedServicesApplication implements CommandLineRunner {
 
     private final ApplicationContext applicationContext;
