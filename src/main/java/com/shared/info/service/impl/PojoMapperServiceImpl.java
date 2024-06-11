8c3158dd8bfd085a69642665b01734a52d45dc94
Suhas H C <suhashc193@gmail.com>
2024-06-11 22:59:21 +0530

    MapStruct implementation

--- src/main/java/com/shared/info/service/impl/PojoMapperServiceImpl.java
+++ src/main/java/com/shared/info/service/impl/PojoMapperServiceImpl.java
@@ -11,9 +11,7 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class PojoMapperServiceImpl implements PojoMapperService {
 
-
     private final ClientCustomerMapper mapper;
-
 
     @Override
     public ClientEntitlement customerEntitlementToClientEntitlement(CustomerEntitlements customerEntitlements) {
--- src/test/java/com/shared/info/controller/ObjectMappingControllerTest.java
+++ src/test/java/com/shared/info/controller/ObjectMappingControllerTest.java
@@ -1,6 +1,5 @@
-package com.shared.info.service.impl;
+package com.shared.info.controller;
 
-import com.shared.info.mapper.ClientCustomerMapper;
 import com.shared.info.pojo.ClientEntitlement;
 import com.shared.info.pojo.CustomerEntitlements;
 import com.shared.info.service.PojoMapperService;
@@ -9,24 +8,23 @@ import org.junit.jupiter.api.Test;
 import static com.shared.info.vo.TestUtils.clientEntitlement;
 import static com.shared.info.vo.TestUtils.customerEntitlements;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
-class PojoMapperServiceImplTest {
+class ObjectMappingControllerTest {
 
-    private final ClientCustomerMapper mapper = mock(ClientCustomerMapper.class);
-    private final PojoMapperService service = new PojoMapperServiceImpl(mapper);
+    private final PojoMapperService service = mock(PojoMapperService.class);
+    private final ObjectMappingController controller = new ObjectMappingController(service);
 
     @Test
     void should_return_client_entitlements_mapped_object_when_customer_entitlement_object_is_passed() {
         var clientEntitlement = clientEntitlement();
         var customerEntitlement = customerEntitlements();
 
-        when(mapper.customerEntitlementToClientEntitlement(customerEntitlement)).thenReturn(clientEntitlement);
-        var response = service.customerEntitlementToClientEntitlement(customerEntitlement);
+        when(service.customerEntitlementToClientEntitlement(customerEntitlement)).thenReturn(clientEntitlement);
+        var response = controller.customerEntitlementToClientEntitlement(customerEntitlement);
         assertThat(response).isInstanceOf(ClientEntitlement.class);
         assertThat(response).isSameAs(clientEntitlement);
-        verify(mapper).customerEntitlementToClientEntitlement(customerEntitlement);
+        verify(service).customerEntitlementToClientEntitlement(customerEntitlement);
     }
 
     @Test
@@ -34,10 +32,11 @@ class PojoMapperServiceImplTest {
         var clientEntitlement = clientEntitlement();
         var customerEntitlement = customerEntitlements();
 
-        when(mapper.clientEntitlementToCustomerEntitlement(clientEntitlement)).thenReturn(customerEntitlement);
-        var response = service.clientEntitlementToCustomerEntitlement(clientEntitlement);
+        when(service.clientEntitlementToCustomerEntitlement(clientEntitlement)).thenReturn(customerEntitlement);
+        var response = controller.clientEntitlementToCustomerEntitlement(clientEntitlement);
         assertThat(response).isInstanceOf(CustomerEntitlements.class);
         assertThat(response).isSameAs(customerEntitlement);
-        verify(mapper).clientEntitlementToCustomerEntitlement(clientEntitlement);
+        verify(service).clientEntitlementToCustomerEntitlement(clientEntitlement);
     }
+
 }
