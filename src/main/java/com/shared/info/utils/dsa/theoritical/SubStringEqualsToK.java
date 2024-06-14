cce35d0e82dc74f79e37e0e579542d8390277962
Suhas H C <suhashc193@gmail.com>
2024-06-14 13:14:21 +0530

    HackerRank questions related to arrays and strings

---
+++ src/main/java/com/shared/info/utils/dsa/theoritical/SubStringEqualsToK.java
@@ -0,0 +1,25 @@
+package com.shared.info.utils.dsa.theoritical;
+
+import java.util.*;
+
+public class SubStringEqualsToK {
+
+    public String smallestSubStringEqualsK(List<String> str, int k) {
+        List<String> matchingStrings = new ArrayList<>(str.size());
+        for (String s : str) {
+            char[] arr = s.toCharArray();
+            int count = 0;
+            for (char c : arr) {
+                if (c == '1') {
+                    count++;
+                }
+                if (count == k) {
+                    matchingStrings.add(s);
+                }
+            }
+        }
+        return matchingStrings.stream()
+                .min(Comparator.comparing(String::length))
+                .get();
+    }
+}
